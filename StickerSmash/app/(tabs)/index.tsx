525783a53d7cfbaa6f6c1e29cbd9f71e86d57fb9
martinfragalouzan06 <martinfragalouzan06@liceolapaz.net>
2025-09-17 11:50:58 +0200

    Añadir imagen de Fondo

--- StickerSmash/app/(tabs)/index.tsx
+++ StickerSmash/app/(tabs)/index.tsx
@@ -1,29 +1,31 @@
-import { Text, View,  StyleSheet } from 'react-native';
-import { Link } from 'expo-router';
+import { View, StyleSheet } from 'react-native';
+ import { Image } from 'expo-image'; 
 
-const Index = () =>  (
+
+const PlaceholderImage = require('@/assets/images/background-image.png');
+
+const Index = () => (
     <View style={styles.container}>
-      <Text style={styles.text}>Home screen</Text>
-      <Link href="/about" style={styles.button}>
-        Go to About screen
-      </Link>
+      <View style={styles.imageContainer}>
+        <Image source={PlaceholderImage} style={styles.image} />
+      </View>
     </View>
-);
+  );
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#25292e',
     alignItems: 'center',
-    justifyContent: 'center',
   },
-  text: {
-    color: '#fff',
+  imageContainer: {
+    flex: 1,
   },
-  button: {
-    fontSize: 20,
-    textDecorationLine: 'underline',
-    color: '#fff',
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
   },
 });
 
